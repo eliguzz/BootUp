f1a4a5d7253296aac55ddf9042a7760197c58740
Eli <eligu@Elis-iMac.local>
2026-05-12 19:14:39 -0400

    initial v2 commit

---
+++ BootUp/BootUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootUpApp.swift
+//  BootUp
+//
+//  Created by Eli on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct BootUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BootUp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BootUp
+//
+//  Created by Eli on 5/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
